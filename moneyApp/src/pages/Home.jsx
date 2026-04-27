00e03264abc52c843a88b85634cd326814e48fec
Donovan McGilbray <djaymcgilbray@gmail.com>
2026-04-27 16:05:37 -0700

    changed home page

--- moneyApp/src/pages/Home.jsx
+++ moneyApp/src/pages/Home.jsx
@@ -2,7 +2,8 @@ import '../App.css'
 import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '../assets/vite.svg'
-import heroImg from '../assets/hero.png'
+import money from '../assets/money.png'
+import Carousel from '../components/Carousel';
 
 export default function Home() {
     const [count, setCount] = useState(0)
@@ -10,15 +11,13 @@ export default function Home() {
       return (
         <section className="page">
           <section id="center">
-            <div className="hero">
-              <img src={heroImg} className="base" width="170" height="179" alt="" />
-              <img src={reactLogo} className="framework" alt="React logo" />
-              <img src={viteLogo} className="vite" alt="Vite logo" />
+            <div className="hero" style={{ marginBottom: "10px" }}>
+              <img src={money} className="base" width="170" height="179" alt="" />
             </div>
-            <div>
+            <div style={{ marginTop: "0" }}>
               <h1>Money App</h1>
               <p>
-                Edit <code>..src/pages/Home.jsx</code> and save to test
+                Track your finances and decide whether you want to invest or pay off debt faster.
               </p>
             </div>
             <button
@@ -32,20 +31,19 @@ export default function Home() {
           <div className="ticks"></div>
     
           <section id="next-steps">
-            <div id="docs">
-              <svg className="icon" role="presentation" aria-hidden="true">
-                <use href="/icons.svg#documentation-icon"></use>
-              </svg>
-              <h2>feel free to add files fwends</h2>
-              <p>we can work on modifying layout week 2 and week 3</p>
-              
+            <div id="docs" style={{ width: "100%" }}>
+              <h2>Learn More</h2>
+              <Carousel />
             </div>
             <div id="social">
-              <svg className="icon" role="presentation" aria-hidden="true">
-                <use href="/icons.svg#social-icon"></use>
-              </svg>
-              <h2>links</h2>
-              <p>git hub repo link (actually works)</p>
+              <h2>About This App</h2>
+              <p>This app is designed to help users better understand their financial situation by 
+                tracking their expenses and managing debt to help you decide to pay off debt or invest. 
+                By tracking your expenses and analyzing your debts, you can see how different payment strategies 
+                affect your payoff time and total interest. It also provides tools to calculate how much you can 
+                earn on investments. With this insight, you can make smarter financial decisions that align with 
+                your goals.
+              </p>
               <ul>
                 <li>
                   <a href="https://github.com/adrimivas/appdev_final.git" target="_blank">
@@ -56,7 +54,7 @@ export default function Home() {
                     >
                       <use href="/icons.svg#github-icon"></use>
                     </svg>
-                    GitHub
+                    View on GitHub
                   </a>
                 </li>
               </ul>
